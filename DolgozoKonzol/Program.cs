754b2f95cb95e8823166b068f0817e87ab4e58ce
Tari Attila <tari.attila@petrik.hu>
2024-03-12 16:19:03 +0100

    Add project files.

---
+++ DolgozoKonzol/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Configuration;
+using System.Text.Json;
+
+namespace DolgozoKonzol
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            string endPoint = ReadSetting("endpointUrl");
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage response = await client.GetAsync(endPoint))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string responseBody = await response.Content.ReadAsStringAsync();
+                            Worker[] workers = JsonSerializer.Deserialize<Worker[]>(responseBody);
+                            int elemekSzama = workers.Length;
+                            Console.WriteLine("Az elemek száma: " + elemekSzama);
+                            string legmagasabbFizetesu = legmagasabbFizetes(workers);
+                            Console.WriteLine("A legmagasabb fizetésű dolgozó: " + legmagasabbFizetesu);
+                            var munkakorok = Munkakor(workers);
+                            foreach (var munkakor in  munkakorok)
+                            {
+                                Console.WriteLine($"Munkakör: {munkakor.Key}, dolgozók száma: {munkakor.Value}");
+                            }
+                        }
+                    }
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+            
+        }
+
+        private static String legmagasabbFizetes(Worker[] worker)
+        {
+            Worker legmagasabbFizetesuDolgozo = worker[0];
+            foreach (var item in worker)
+            {
+                if(item.Salary > legmagasabbFizetesuDolgozo.Salary)
+                {
+                    legmagasabbFizetesuDolgozo = item;
+                }
+            }
+            return legmagasabbFizetesuDolgozo.Name;
+        }
+       
+
+        public static string ReadSetting(string keyName)
+        {
+            string result = null;
+            try
+            {
+                var value = ConfigurationManager.AppSettings;
+                result = value[keyName];
+            }
+            catch (ConfigurationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+
+        static Dictionary<string, int> Munkakor(Worker[] workers)
+        {
+            var szamol = new Dictionary<string, int>();
+
+            foreach (var worker in workers)
+            {
+                if (!szamol.ContainsKey(worker.Position))
+                {
+                    szamol[worker.Position] = 0;
+                }
+                szamol[worker.Position]++;
+            }
+
+            return szamol;
+        }
+    }
+}
---
+++ DolgozoKonzol/˙Worker.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DolgozoKonzol
+{
+    public partial class Worker
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("salary")]
+        public int Salary { get; set; }
+
+        [JsonPropertyName("position")]
+        public string Position { get; set; }
+
+       
+        public override string? ToString()
+        {
+            return Name;
+        }
+    }
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+    
+}
